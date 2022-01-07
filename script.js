a35f8b5aaaabbca97f4f42135a08130f2fd1d14d
Ayers, Kevin Andrew <kayers8@gatech.edu>
2022-01-06 17:24:03 -0800

    added progress bar animation

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -85,6 +87,17 @@ function nextSong() {
     playSong();
 }
 
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const {duration, currentTime } = e.srcElement;
+
+        // Update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
 
 // ON Load - Select First Song
 loadSong(songs[songIndex]);
@@ -92,3 +105,4 @@ loadSong(songs[songIndex]);
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
